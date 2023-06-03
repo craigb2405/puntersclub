d5cad568c4fbd2df1d90beffcdc78883744c3b90
Craig <craig.butler@jrbsolutions.co.uk>
2023-06-03 11:51:39 +0100

    began connection of frontend to backend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+  const [bets, setBets] = useState(null)
+
+  useEffect(() => {
+    const fetchBets = async () => {
+      const response = await fetch('http://localhost:4000/api/bets')
+      const json = await response.json()
+
+      if (response.ok) {
+        setBets(json)
+
+      }
+
+    }
+    fetchBets()
+
+  }, [])
   return (
-    <div className='home'><h2>Home</h2></div>
+    <div className='home'>
+      <div className='bets'>
+        {bets && bets.map(() => (
+          <p key={bets._id}>{bets.punter}</p>
+        ))}
+      </div>
+    </div>
   )
 }
 
